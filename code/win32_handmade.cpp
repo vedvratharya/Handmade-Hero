163c7aa9ec24bb31828a266e358235ba594f01c9
vedvratharya <vedvratharya17@gmail.com>
2024-06-25 21:56:32 +0530

    First commit. Looks good so far. Completed about 3 hours and spent uncounted (probably 7+) hours. Getting to know everything slowly. Comments in the file itself, will try to make this more refined.

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 
+// define to keep track of static variables using the name.
 #define internal static
 #define local_persist static
 #define global_variable static
@@ -7,13 +8,59 @@
 // TODO: This is a global for now.
 global_variable bool Running;
 
-internal void ResizeDIBSection(int Width, int Height)
-{
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
+//Renaming all names with Win32 to avoid confusion with Windows actual function names.
+internal void 
+Win32ResizeDIBSection(int Width, int Height)
+{
+	// TODO: Bulletproof this.
+	// Maybe don't free first, free after, then free first if that fails.
+
+	if (BitmapHandle)
+	{
+		DeleteObject(BitmapHandle);
+	}
+
+	if (!BitmapDeviceContext)
+	{
+		// TODO: Should we recreate this under certain special circumstances.
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	BitmapHandle = CreateDIBSection(
+		BitmapDeviceContext, &BitmapInfo,
+		DIB_RGB_COLORS,
+		&BitmapMemory,
+		0, 0);
 }
 
-LRESULT CALLBACK MainWindowCallback(
-//		WNDPROC PrevWndFunc,
+internal void 
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+	StretchDIBits(
+		DeviceContext, 
+		X, Y, Width, Height, // Destination values
+		X, Y, Width, Height, // Source Values
+		BitmapMemory,
+		&BitmapInfo,
+		DIB_RGB_COLORS, // Table containing literal RGB Values
+		SRCCOPY); // Coies directly from source to destination rectangle.
+}
+
+LRESULT CALLBACK 
+Win32MainWindowCallback(
+	//		WNDPROC PrevWndFunc,
 	HWND Window,
 	UINT Message,
 	WPARAM WParam,
@@ -29,8 +76,7 @@ LRESULT CALLBACK MainWindowCallback(
 			GetClientRect(Window, &ClientRect);
 			LONG Width = ClientRect.right - ClientRect.left;
 			LONG Height = ClientRect.bottom - ClientRect.top;
-			ResizeDIBSection(Width, Height);
-			OutputDebugStringA("WM_SIZE\n");
+			Win32ResizeDIBSection(Width, Height);
 		} break;
 
 		case WM_DESTROY:
@@ -58,16 +104,7 @@ LRESULT CALLBACK MainWindowCallback(
 			int Y = Paint.rcPaint.top;
 			LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
 			LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-			local_persist DWORD Operation = WHITENESS;
-			PatBlt(DeviceContext, X, Y, Height, Width, Operation);
-			if (Operation == WHITENESS)
-			{
-				Operation = BLACKNESS;
-			}
-			else
-			{
-				Operation = WHITENESS;
-			}
+			Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
 			EndPaint(Window, &Paint);
 		} break;
 
@@ -94,7 +131,7 @@ int CALLBACK WinMain(
 
 	// TODO: check if CS_OWNDC|CS_HREDRAW|CS_VREDRAW still matter
 	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
 	WindowClass.hInstance = Instance;
 //		WindowClass.hIcon;
 	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
