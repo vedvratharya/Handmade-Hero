6ffb0473fed66809e1def4549a5f5e67e76a6e3e
vedvratharya <vedvratharya17@gmail.com>
2024-07-10 19:31:43 +0530

    added O2 to increase performance of generated code.

--- code/build.bat
+++ code/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -FC -Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
+cl -O2 -FC -Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
