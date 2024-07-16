86df2e60b984e0577a3c513fa0d8865db83f9e2a
vedvratharya <vedvratharya17@gmail.com>
2024-07-16 22:06:44 +0530

    implemented platform-independent debug file I/O

--- code/handmade.h
+++ code/handmade.h
@@ -27,9 +27,34 @@
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 // TODO: swap min, max ... macros???
 
+inline uint32
+SafeTruncateUInt64(int64 Value)
+{
+	// TODO: Define for maximum values
+	Assert(Value <= 0xFFFFFFFF);
+	uint32 Result = (uint32)Value;
+	return(Result);
+}
+
 /*
 	TODO: Services that the platform layer provides to the game.
 */
+#if HANDMADE_INTERNAL
+/*
+	IMPORTANT:
+
+	These are NOT for doing anything in the shipping game - they are blocking and the write doesn't protect 
+	against lost data!
+*/
+struct debug_read_file_result
+{
+	uint32 ContentsSize;
+	void* Contents;
+};
+internal debug_read_file_result DEBUGPlatformReadEntireFile(char* Filename);
+internal void DEBUGPlatformFreeFileMemory(void* Memory);
+internal bool32 DEBUGPlatformWriteEntireFile(char* Filename, uint32 MemorySize, void* Memory);
+#endif
 
 /*
 	NOTE: Services that the game provides to the platform layer.
