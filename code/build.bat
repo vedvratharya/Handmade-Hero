5cfd770f3c0972f643e11a1c78742e4965ac8219
vedvratharya <vedvratharya17@gmail.com>
2024-07-21 18:01:13 +0530

    Some compiler switches and treat warning as error. Also ignore some warnings.

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,8 @@
 @echo off
 
+REM TODO - can we just build both with one exe?
+
 IF NOT EXIST ..\..\build mkdir ..\..\build
 pushd ..\..\build
-cl -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
+cl -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 -Fmwin32_handmade.map ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
